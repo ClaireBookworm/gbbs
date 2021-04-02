5ec7994d0f6ed26c9a92f111e8cc7f0a4c332e26
Michael Huang <michaelyhuang23@gmail.com>
2021-04-01 22:25:00 -0400

    format

--- benchmarks/BFS/NonDeterministicBFS/BFS.h
+++ benchmarks/BFS/NonDeterministicBFS/BFS.h
@@ -25,42 +25,51 @@
 
 #include "gbbs/gbbs.h"
 
-namespace gbbs {
+namespace gbbs
+{
 
-template <class W>
-struct BFS_F {
-  uintE* Parents;
-  BFS_F(uintE* _Parents) : Parents(_Parents) {}
-  inline bool update(uintE s, uintE d, W w) {
-    if (Parents[d] == UINT_E_MAX) {
-      Parents[d] = s;
-      return 1;
-    } else {
-      return 0;
+  template <class W>
+  struct BFS_F
+  {
+    uintE *Parents;
+    BFS_F(uintE *_Parents) : Parents(_Parents) {}
+    inline bool update(uintE s, uintE d, W w)
+    {
+      if (Parents[d] == UINT_E_MAX)
+      {
+        Parents[d] = s;
+        return 1;
+      }
+      else
+      {
+        return 0;
+      }
     }
-  }
-  inline bool updateAtomic(uintE s, uintE d, W w) {
-    return (pbbslib::atomic_compare_and_swap(&Parents[d], UINT_E_MAX, s));
-  }
-  inline bool cond(uintE d) { return (Parents[d] == UINT_E_MAX); }
-};
+    inline bool updateAtomic(uintE s, uintE d, W w)
+    {
+      return (pbbslib::atomic_compare_and_swap(&Parents[d], UINT_E_MAX, s));
+    }
+    inline bool cond(uintE d) { return (Parents[d] == UINT_E_MAX); }
+  };
 
-template <class Graph>
-inline sequence<uintE> BFS(Graph& G, uintE src) {
-  using W = typename Graph::weight_type;
-  /* Creates Parents array, initialized to all -1, except for src. */
-  auto Parents = sequence<uintE>(G.n, [&](size_t i) { return UINT_E_MAX; });
-  Parents[src] = src;
+  template <class Graph>
+  inline sequence<uintE> BFS(Graph &G, uintE src)
+  {
+    using W = typename Graph::weight_type;
+    /* Creates Parents array, initialized to all -1, except for src. */
+    auto Parents = sequence<uintE>(G.n, [&](size_t i) { return UINT_E_MAX; });
+    Parents[src] = src;
 
-  vertexSubset Frontier(G.n, src);
-  size_t reachable = 0;
-  while (!Frontier.isEmpty()) {
-    std::cout << Frontier.size() << "\n";
-    reachable += Frontier.size();
-    Frontier = edgeMap(G, Frontier, BFS_F<W>(Parents.begin()), -1, sparse_blocked | dense_parallel);
+    vertexSubset Frontier(G.n, src);
+    size_t reachable = 0;
+    while (!Frontier.isEmpty())
+    {
+      std::cout << Frontier.size() << "\n";
+      reachable += Frontier.size();
+      Frontier = edgeMap(G, Frontier, BFS_F<W>(Parents.begin()), -1, sparse_blocked | dense_parallel);
+    }
+    std::cout << "Reachable: " << reachable << "\n";
+    return Parents;
   }
-  std::cout << "Reachable: " << reachable << "\n";
-  return Parents;
-}
 
-}  // namespace gbbs
+} // namespace gbbs
