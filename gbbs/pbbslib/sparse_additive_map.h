db2b35faf94065e4854879c035537b0613d6a4d6
Jessica Shi <jessicashi@gmail.com>
2021-04-07 23:44:58 -0400

    additive map fix

--- gbbs/pbbslib/sparse_additive_map.h
+++ gbbs/pbbslib/sparse_additive_map.h
@@ -93,10 +93,10 @@ class sparse_additive_map {
     while (1) {
       if (std::get<0>(table[h]) == empty_key) {
         if (pbbslib::CAS(&std::get<0>(table[h]), empty_key, k)) {
-          std::get<1>(table[h]) = std::get<1>(kv);
+          pbbslib::write_add(&std::get<1>(table[h]), v);
           return 1;
         }
-      }
+      }          
       if (std::get<0>(table[h]) == k) {
         pbbslib::write_add(&std::get<1>(table[h]), v);
         return false;
