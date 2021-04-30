4bd15d7c07838ccf236242578595ae471bad5ea4
Claire Wang <clairebookworm@Claires-Batcave.lan1>
2021-04-30 07:48:09 -0700

    read txt file

--- utils/konect-converter.py
+++ utils/konect-converter.py
@@ -1,4 +1,15 @@
 # converts the graph data files from http://konect.cc/networks/ into a file to be inputted into snap converter
 
-import file
+# import file
 import pandas
+
+## file input
+f = open("graphfile.txt", "r") 
+print(f.read())
+
+rows = []
+numbers = {} # dictionary for key value?
+
+for row in f:
+    rows += row;
+
