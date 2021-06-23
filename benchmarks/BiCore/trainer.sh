43a97770a29b81b7530e39902cd7221d8039d509
Michael Huang <michaelyhuang23@gmail.com>
2021-06-23 04:06:53 +0000

    dblp, arxiv, reuters

--- benchmarks/BiCore/trainer.sh
+++ benchmarks/BiCore/trainer.sh
@@ -1,43 +1,73 @@
+export NUM_THREADS=60 &&
+make &&
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj
+export NUM_THREADS=30 &&
+make &&
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj
 export NUM_THREADS=16 &&
 make &&
-./BiCore -s -bi 395978 -rounds 10 ../../inputs/flickr_adj
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj
 export NUM_THREADS=8 &&
 make &&
-./BiCore -s -bi 395978 -rounds 10 ../../inputs/flickr_adj
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj
 export NUM_THREADS=4 &&
 make &&
-./BiCore -s -bi 395978 -rounds 10 ../../inputs/flickr_adj
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj
 export NUM_THREADS=2 &&
 make &&
-./BiCore -s -bi 395978 -rounds 10 ../../inputs/flickr_adj
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj
 unset NUM_THREADS &&
 export SERIAL=1 &&
 make &&
-./BiCore -s -bi 395978 -rounds 10 ../../inputs/flickr_adj
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj
 
 unset SERIAL &&
 unset NUM_THREADS &&
 make &&
-./BiCore -s -bi 556076 -rounds 10 ../../inputs/trec_adj
+./BiCore -s -bi 16725 -rounds 10 ../../inputs/arXiv_adj
 
 export NUM_THREADS=30 &&
 make &&
-./BiCore -s -bi 556076 -rounds 10 ../../inputs/trec_adj
+./BiCore -s -bi 16725 -rounds 10 ../../inputs/arXiv_adj
 export NUM_THREADS=16 &&
 make &&
-./BiCore -s -bi 556076 -rounds 10 ../../inputs/trec_adj
+./BiCore -s -bi 16725 -rounds 10 ../../inputs/arXiv_adj
 export NUM_THREADS=8 &&
 make &&
-./BiCore -s -bi 556076 -rounds 10 ../../inputs/trec_adj
+./BiCore -s -bi 16725 -rounds 10 ../../inputs/arXiv_adj
 export NUM_THREADS=4 &&
 make &&
-./BiCore -s -bi 556076 -rounds 10 ../../inputs/trec_adj
+./BiCore -s -bi 16725 -rounds 10 ../../inputs/arXiv_adj
 export NUM_THREADS=2 &&
 make &&
-./BiCore -s -bi 556076 -rounds 10 ../../inputs/trec_adj
+./BiCore -s -bi 16725 -rounds 10 ../../inputs/arXiv_adj
+unset NUM_THREADS &&
+export SERIAL=1 &&
+make &&
+./BiCore -s -bi 16725 -rounds 10 ../../inputs/arXiv_adj
+
+unset SERIAL &&
+unset NUM_THREADS &&
+make &&
+./BiCore -s -bi 1953084 -rounds 5 ../../inputs/dblp_adj
+
+export NUM_THREADS=30 &&
+make &&
+./BiCore -s -bi 1953084 -rounds 5 ../../inputs/dblp_adj
+export NUM_THREADS=16 &&
+make &&
+./BiCore -s -bi 1953084 -rounds 5 ../../inputs/dblp_adj
+export NUM_THREADS=8 &&
+make &&
+./BiCore -s -bi 1953084 -rounds 5 ../../inputs/dblp_adj
+export NUM_THREADS=4 &&
+make &&
+./BiCore -s -bi 1953084 -rounds 5 ../../inputs/dblp_adj
+export NUM_THREADS=2 &&
+make &&
+./BiCore -s -bi 1953084 -rounds 5 ../../inputs/dblp_adj
 
 unset NUM_THREADS &&
 export SERIAL=1 &&
 make &&
-./BiCore -s -bi 556076 -rounds 10 ../../inputs/trec_adj
-
+./BiCore -s -bi 1953084 -rounds 5 ../../inputs/dblp_adj
