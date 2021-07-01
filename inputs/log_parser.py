a2c74bd5d5b0e5c64359c3bcbde1cb1b209e8db3
ClaireBookworm <clara32356@gmail.com>
2021-07-01 12:19:47 -0400

    larser

--- inputs/log_parser.py
+++ inputs/log_parser.py
@@ -1,35 +1,41 @@
 import fileinput
 import csv
 
-linesA = [[]]
-linesB = [[]]
-rhoA = []
-rhoB = []
-timesA = []
-timesB = []
-
+# linesA = [[]]
+# linesB = [[]]
+lines = [[]]
+# rhoA = []
+# rhoB = []
+# timesA = []
+# timesB = []
+rho = []
+times = []
+sum = 0
+count = 0
 
 filein = input("Log File Name (no extension): ")
 
 for line in fileinput.input(files=filein):
 	line = line[:-1]
-	# lines.append(line)
+	lines.append(line)
 	if ("total" in line):
 		print("total:" + line)
+		sum += float(line.split(' ')[1])
+		count += 1
 		continue;
 	print(line.split(' '))
-	if line.split(' ')[0] == "coreA":
-		linesA.append(line.split(' '))
-		rhoA.append(line.split(' ')[2])
-		timesA.append(line.split(' ')[4])
-	elif line.split(' ')[0] == "coreB":
-		linesB.append(line.split(' '))
-		rhoB.append(line.split(' ')[2])
-		timesB.append(line.split(' ')[4])
-	else:
-		"Error"
-	# rho.append(line.split(' ')[2])
-	# times.append(line.split(' ')[4])
+	# if line.split(' ')[0] == "coreA":
+	# 	linesA.append(line.split(' '))
+	# 	rhoA.append(line.split(' ')[2])
+	# 	timesA.append(line.split(' ')[4])
+	# elif line.split(' ')[0] == "coreB":
+	# 	linesB.append(line.split(' '))
+	# 	rhoB.append(line.split(' ')[2])
+	# 	timesB.append(line.split(' ')[4])
+	# else:
+	# 	"Error"
+	rho.append(line.split(' ')[2])
+	times.append(line.split(' ')[4])
 
 graph_title = input("Enter Graph Title: ")
 
@@ -43,6 +49,9 @@ def write_results(file, xlabel, ylabel):
 			core_writer.writerow((xlabel[num],ylabel[num]))
 
 # print(lines)
-
-write_results(graph_title + "-A.csv", rhoA, timesA)
-write_results(graph_title + "-B.csv", rhoB, timesB)
+write_results(graph_title + ".csv", rho, times)
+print(sum)
+print (sum / float(count))
+print ("Count: " + count)
+# write_results(graph_title + "-A.csv", rhoA, timesA)
+# write_results(graph_title + "-B.csv", rhoB, timesB)
