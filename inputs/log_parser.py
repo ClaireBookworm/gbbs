5fcc106cbc28db40a5a798fd3dbe10a25741dcb5
Michael Huang <michaelyhuang23@gmail.com>
2021-07-02 03:36:42 -0400

    new parser

--- inputs/log_parser.py
+++ inputs/log_parser.py
@@ -2,73 +2,73 @@ import fileinput
 import csv
 import os
 
-# linesA = [[]]
-# linesB = [[]]
-lines = [[]]
-# rhoA = []
-# rhoB = []
-# timesA = []
-# timesB = []
-rho = []
-times = []
-sum = 0
-count = 0
-deltaC = 0
-stopped = False
 print(os.getcwd())
 filein = input("Log File Name (no extension): ")
+for k in [1,2,4,8,16,30,60]:
+	# linesA = [[]]
+	# linesB = [[]]
+	lines = [[]]
+	# rhoA = []
+	# rhoB = []
+	# timesA = []
+	# timesB = []
+	rho = []
+	times = []
+	sum = 0
+	count = 0
+	deltaC = 0
+	stopped = False
+	filename1 = filein+"_adj_"+str(k)+"_threads.txt"
+	filename2 = filein+"_"+str(k)+".error"
+	filename3 = "../benchmarks/BiCore/"+filein+"_"+str(k)+".error"
+	fin = None
+	if os.path.isfile(filename1):
+		fin = fileinput.input(files = filename1)
+	elif os.path.isfile(filename2):
+		fin = fileinput.input(files = filename2)
+	elif os.path.isfile(filename3):
+		fin = fileinput.input(files = filename3)
+	else:
+		print("no such file")
+		exit(0)
+	for line in fin:
+		line = line[:-1]
+		if "coreA" not in line and "coreB" not in line and "total" not in line and "time per iter" not in line:
+			continue
+		lines.append(line)
+		if "coreA" in line and not stopped:
+			deltaC+=1
+		if "coreB" in line:
+			stopped=True
+		if ("total" in line):
+			sum += float(line.split(' ')[1])
+			count += 1
+			continue
+		if "time per iter" in line:
+			sum += float(line.split(' ')[-1])
+			print(sum)
+			count += 1
+			continue
+		rho.append(line.split(' ')[2])
+		times.append(line.split(' ')[4])
+	print(k)
+	print ("Rho max: "+str(max(rho)))
+	print ("delta: "+str(deltaC))
 
-for line in fileinput.input(files=filein):
-	line = line[:-1]
-	if "coreA" not in line and "coreB" not in line and "total" not in line:
-		continue
-	lines.append(line)
-	if "coreA" in line and not stopped:
-		deltaC+=1
-	if "coreB" in line:
-		stopped=True
-	if ("total" in line):
-		print("total:" + line)
-		sum += float(line.split(' ')[1])
-		count += 1
-		continue
-	if "time per iter" in line:
-		print("total:" + line)
-		sum += float(line.split(' ')[1])
-		count += 1
-		continue
-	print(line.split(' '))
-	# if line.split(' ')[0] == "coreA":
-	# 	linesA.append(line.split(' '))
-	# 	rhoA.append(line.split(' ')[2])
-	# 	timesA.append(line.split(' ')[4])
-	# elif line.split(' ')[0] == "coreB":
-	# 	linesB.append(line.split(' '))
-	# 	rhoB.append(line.split(' ')[2])
-	# 	timesB.append(line.split(' ')[4])
-	# else:
-	# 	"Error"
-	rho.append(line.split(' ')[2])
-	times.append(line.split(' ')[4])
+	graph_title = filein+"-"+str(k)+"-rho-time"
 
-print ("Rho max: "+str(max(rho)))
-print ("delta: "+str(deltaC))
+	def write_results(file, xlabel, ylabel):
+		x_label = "rho"
+		y_label = "time"
+		with open(file, mode='w') as origin:
+			core_writer = csv.writer(origin, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			core_writer.writerow((x_label,y_label))
+			for num in range(0, len(xlabel)):
+				core_writer.writerow((xlabel[num],ylabel[num]))
 
-graph_title = input("Enter Graph Title: ")
-
-def write_results(file, xlabel, ylabel):
-	x_label = input("X Label: ")
-	y_label = input("Y Label: ")
-	with open(file, mode='w') as origin:
-		core_writer = csv.writer(origin, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		core_writer.writerow((x_label,y_label))
-		for num in range(0, len(xlabel)):
-			core_writer.writerow((xlabel[num],ylabel[num]))
-
-# print(lines)
-write_results(graph_title + ".csv", rho, times)
-print(sum)
-print (sum / float(count))
-print ("Count: " + str(count))
-# write_results(graph_title + "-A.csv", rhoA, timesA)
-# write_results(graph_title + "-B.csv", rhoB, timesB)
+	# print(lines)
+	write_results(graph_title + ".csv", rho, times)
+	print(sum)
+	print ("avg time: "+str(sum / float(count)))
+	# write_results(graph_title + "-A.csv", rhoA, timesA)
+	# write_results(graph_title + "-B.csv", rhoB, timesB)
