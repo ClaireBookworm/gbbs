9755dd716b4ca7076537798d8ad11056072a750a
Michael Huang <michaelyhuang23@gmail.com>
2021-07-27 22:47:43 -0400

    change interleave

--- benchmarks/BiCore/BiCore.h
+++ benchmarks/BiCore/BiCore.h
@@ -87,12 +87,11 @@ namespace gbbs
 		parallel_for_alloc<PeelingMemory>(init_f, finish_f, 1,delta*2+1,[&](size_t core, PeelingMemory* mem){
 			timer t_in; t_in.start();
 			mem->init();
-			if(core%2==1){
-				core = (core+1)/2;
+			if(core<=delta){
 				auto retA = PeelFixA(G, BetaMax, AlphaMax, core, bipartition, num_buckets, mem);
 				msgA[core]=std::make_tuple(std::get<0>(retA),std::get<1>(retA),t_in.stop());
 			}else{
-				core /= 2;
+				core -= delta;
 				auto retB = PeelFixB(G, BetaMax, AlphaMax, core, bipartition, num_buckets, mem);
 				msgB[core]=std::make_tuple(std::get<0>(retB),std::get<1>(retB),t_in.stop());
 			}
