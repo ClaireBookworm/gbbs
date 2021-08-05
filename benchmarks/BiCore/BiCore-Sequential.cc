bd4350ce3231b494fac114d101d848e715464822
clairebookworm <clara32356@gmail.com>
2021-08-04 20:47:50 -0700

    seq

--- benchmarks/BiCore/BiCore-Sequential.cc
+++ benchmarks/BiCore/BiCore-Sequential.cc
@@ -1,7 +1,9 @@
 #include <fstream>
 #include <iostream>
 
-template <class Graph> inline void BiCore(Graph &G) {
+template <class Graph>
+inline void BiCore(Graph &G, size_t num_buckets = 16, size_t bipartition = 2, size_t peel_core_alpha = 0, size_t peel_core_beta = 0)
+{
     std::cout << "begin" << std::endl;
     auto G = graph; // imagine this is the graph you want to run the algorithm on
     // take in the graph and find any vertices with degree less than max alpha or beta
@@ -10,37 +12,46 @@ template <class Graph> inline void BiCore(Graph &G) {
     // repeat until all vertices have degree less than max alpha or beta
     size_t max_alpha = 0;
     size_t max_beta = 0;
-    for (auto v : G.vertices()) {
-        if (G.degree(v) > max_alpha) {
+    for (auto v : G.vertices())
+    {
+        if (G.degree(v) > max_alpha)
+        {
             // max_alpha = G.degree(v);
         }
     }
 
     auto ret = KCore(G, num_buckets);
-	const uintE delta = static_cast<size_t>(pbbslib::reduce_max(ret));
+    const uintE delta = static_cast<size_t>(pbbslib::reduce_max(ret));
     // delta is the max unicore
-    auto PeelFixAllA = [&]() {
-        for (int i = 0; i < delta+1; ++i) {
-            timer t_in; t_in.start();
+    auto PeelFixAllA = [&]()
+    {
+        for (int i = 0; i < delta + 1; ++i)
+        {
+            timer t_in;
+            t_in.start();
             auto retA = PeelFixA(G, BetaMax, AlphaMax, core, bipartition, num_buckets);
+        }
     }
 }
-template <class Graph> inline std::pair<size_t, size_t> PeelFixA (Graph &G, size_t max_beta, size_t max_alpha, size_t core, size_t bipartition = 2, size_t num_buckets = 16) {
+template <class Graph>
+inline std::pair<size_t, size_t> PeelFixA(Graph &G, size_t max_beta, size_t max_alpha, size_t core, size_t bipartition = 2, size_t num_buckets = 16)
+{
     timer bt, ft, pt; /// bt: begin time, ft: finish time, pt: processing time
     pt.start();
     // get degree of all vertices in A and delete all with degree < max_alpha
     // then, decrement degree of all neighboring vertices in A by 1
-    for (auto v : G.vertices()) {
-        if (G.get_vertex(v).out_degree() < max_alpha) {
+    for (auto v : G.vertices())
+    {
+        if (G.get_vertex(v).out_degree() < max_alpha)
+        {
             G.remove_vertex(v);
             // ngh count
         }
     }
-    auto msgA="complete PeelFixA";
-
+    auto msgA = "complete PeelFixA";
 }
 
-template <class Graph> inlie nghCount (Graph &G) {
-
+template <class Graph>
+inlie nghCount(Graph &G)
+{
 }
-
