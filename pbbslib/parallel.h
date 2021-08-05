80453041bfc0aea243a43ae5729bc9132cce9339
Michael Huang <michaelyhuang23@gmail.com>
2021-08-04 21:44:13 -0400

    'debug'

--- pbbslib/parallel.h
+++ pbbslib/parallel.h
@@ -229,9 +229,10 @@ inline void parallel_for_alloc(Af init_alloc, Df finish_alloc, long start,
                                bool conservative) {
   parallel_for(start, end,
                [&](long i) {
-                  static thread_local std::unique_ptr<A> alloc(new A());
-                  init_alloc(alloc.get());
-                  f(i, alloc.get());
+                  //static thread_local std::unique_ptr<A> alloc(new A());
+                static thread_local A* alloc = new A();
+                  init_alloc(alloc);
+                  f(i, alloc);
                },
                granularity, conservative);
   //finish_alloc(alloc);
