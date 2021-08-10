7044cb5a355cfa5ba3ff07b3ddfa992843796e1f
clairebookworm <clara32356@gmail.com>
2021-08-10 14:03:20 -0700

    temp commit

--- benchmarks/BiCore/serial-test/bicore-serial.cpp
+++ benchmarks/BiCore/serial-test/bicore-serial.cpp
@@ -3,7 +3,8 @@
 #include <utility>
 #include <algorithm>
 
-std::pair<int, int> getPairs(std::string fileName) {
+std::pair<int, int> getPairs(std::string fileName)
+{
 	std::ifstream fin(fileName);
 	std::string graphType;
 	fin >> graphType;
@@ -12,8 +13,8 @@ std::pair<int, int> getPairs(std::string fileName) {
 
 	auto p = new std::pair<int, int>[u + w];
 	// std::pair<int, int> p = new std::pair[u + w];
-	fin >> u >> w;// sizes of bipartite node groups
-	int** all = new int*[u+w];
+	fin >> u >> w; // sizes of bipartite node groups
+	int **all = new int *[u + w];
 	int uGraph[u];
 	for (int i = 0; i < u; i++)
 	{
@@ -36,18 +37,21 @@ std::pair<int, int> getPairs(std::string fileName) {
 	{
 		// p[pairCount].first = uGraph[i];
 		// int topVal = uGraph[i];
-		std::cout << "VALUE OF U: " << uGraph[i] << std::endl;
-		for (int j = 0; j < w; j++) {
+		// std::cout << "VALUE OF U: " << uGraph[i] << std::endl;
+		for (int j = 0; j < w; j++)
+		{
 			if (wGraph[j] > 0)
 			{
 				p[pairCount].first = *all[i];
-				p[pairCount].second = *all[u+j];
+				p[pairCount].second = *all[u + j];
 				// std::cout << "looking at w: " << wGraph[j] << std::endl;
 				wGraph[j]--;
 				uGraph[i]--;
 				pairCount++;
 				// continue;
-			} else {
+			}
+			else
+			{
 				continue;
 			}
 			if (uGraph[i] <= 0)
@@ -68,6 +72,24 @@ std::pair<int, int> getPairs(std::string fileName) {
 	return *p;
 }
 
-int main() {
-	getPairs("test-graph.txt");
+// use max alpha and beta
+template <class Graph>
+inline void BiCore(Graph &G, size_t bipartition = 2, size_t peel_core_alpha = 0, size_t peel_core_beta = 0)
+{
+	std::cout << "starting" << std::endl;
+	const size_t n = G.n;					// # of vertices
+	const size_t n_b = n - bipartition - 1; // number of vertices in first partition
+	const size_t n_a = bipartition + 1;		// number of vertices in second partition
+
+	// alphamax is max degree in first partition
+	auto AlphaMax = sequence<sequence<size_t>>(n_b, [&G, &n_a](size_t i){ return sequence<size_t>(1+G.get_vertex(i+n_a).out_degree(),[](size_t i){return 0;}); });
+	// BetaMax[u][A]
+	auto BetaMax = sequence<sequence<size_t>>(n_a, [&G](size_t i){ return sequence<size_t>(1+G.get_vertex(i).out_degree(),[](size_t i){return 0;}); });
+
+	for (v : G.vertices()) {
+		if (v.get_degree < AlphaMax) {
+			G.remove(v);
+		}
+	}
 }
+
