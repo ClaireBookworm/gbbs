9224749a6b32cfa73105f514006c8dfbf7f96689
Michael Huang <michaelyhuang23@gmail.com>
2021-09-08 21:18:08 -0400

    prototype complete

--- benchmarks/BiCore/BiCore.h
+++ benchmarks/BiCore/BiCore.h
@@ -64,11 +64,100 @@ inline void BiCore(Graph &G, size_t num_buckets = 16, size_t bipartition = 2, ui
 	const size_t n_b = n - bipartition - 1; // number of vertices in second partition
 	auto ret = KCore(G, num_buckets);
 	const uintE delta = static_cast<uintE>(pbbslib::reduce_max(ret));
-		
+	double slope = 1.5;
+	double thread_ratio = 1; //each worker gets assigned thread_ratio/num_workers() percent of depth
+	double avgSpan = (slope+1)/2*delta/num_workers()*thread_ratio;
+	double curSpan = 0;
+	std::vector<size_t> breakptrs;
+	breakptrs.push_back(0);
+	for(size_t i=1; i<=delta; i++){
+		curSpan += slope-(slope-1)/delta*i;
+		if(curSpan>=avgSpan*1){
+			curSpan = 0;
+			breakptrs.push_back(i);
+			std::cout<<"breakptr at "<<i<<std::endl;
+		}
+	}
+	if(breakptrs[breakptrs.size()-1]!=delta)
+		breakptrs.push_back(delta);
+	std::cout<<"delta "<<delta<<" size "<<breakptrs.size()<<std::endl;
+
+	sequence<uintE>* prepeelA = new sequence<uintE>[breakptrs.size()];
+	par_for(1,breakptrs.size(),1,[&](size_t idx){
+		uintE minCore = breakptrs[idx-1]+1;
+		sequence<uintE> degA(n, [&](size_t i) { return G.get_vertex(i).out_degree(); });
+		size_t initSize = pbbslib::reduce_add(sequence<uintE>(n, [&](size_t i) {return degA[i]<minCore;}));
+		std::vector<uintE> DelA(initSize);
+		for(size_t i=0, j=0; i<n; i++) if(degA[i]<minCore) DelA[j++]=i;
+		initialClean(G, degA, DelA, minCore);
+		prepeelA[idx] = std::move(degA);
+	});
+
+	sequence<uintE>* prepeelB = new sequence<uintE>[breakptrs.size()];
+	par_for(1,breakptrs.size(),1,[&](size_t idx){
+		uintE minCore = breakptrs[idx-1]+1;
+		sequence<uintE> degB(n, [&](size_t i) { return G.get_vertex(i).out_degree(); });
+		size_t initSize = pbbslib::reduce_add(sequence<uintE>(n, [&](size_t i) {return degB[i]<minCore;}));
+		std::vector<uintE> DelB(initSize);
+		for(size_t i=0, j=0; i<n; i++) if(degB[i]<minCore) DelB[j++]=i;
+		initialClean(G, degB, DelB, minCore);
+		prepeelB[idx] = std::move(degB);
+	});
+
+	sequence<uintE>* prepeelB = new sequence<uintE>[breakptrs.size()];
+	par_for(1,breakptrs.size(),1,[&](size_t idx){
+		uintE minCore = breakptrs[idx-1]+1;
+		sequence<uintE> degB(n, [&](size_t i) {
+			return G.get_vertex(i).out_degree();
+		});
+		size_t initSize = pbbslib::reduce_add(sequence<uintE>(n_b, [&](size_t i) {return degB[i+n_a]<minCore;}));
+		pbbslib::dyn_arr<uintE> DelB(initSize);
+		for(size_t i=n_a; i<n; i++) if(degB[i]<minCore) DelB.push_back(i);
+		initialClean(G, degB, DelB, minCore);
+		prepeelB[idx] = std::move(degB);
+	});
+
+	par_for(1,breakptrs.size(),1,[&](size_t idx){
+		std::cout<<"running range "<<breakptrs[idx-1]+1<<" to "<<breakptrs[idx]<<std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		uintE minCore = breakptrs[idx-1]+1;
+
+		auto peelAllFixA = [&](){
+		par_for(breakptrs[idx-1]+1, breakptrs[idx]+1, 1, [&](size_t core){
+			timer t_in; t_in.start();
+			sequence<uintE> D = prepeelA[idx];
+			size_t initSize = pbbslib::reduce_add(sequence<uintE>(n, [&](size_t i) { return (D[i]<core) & (D[i]>=minCore); }));
+			std::vector<uintE> delA(initSize);
+			for(size_t i = 0, j = 0; i<n; i++) if((D[i]<core) && (D[i]>=minCore)) delA[j++]=i; 
+			initialClean(G, D, delA, core);
+
+			auto ret = PeelFixA(G, D, core, n_a, n_b, num_buckets);
+			timeA[core-1] = std::get<1>(ret);
+			t_in.stop();
+			tTime[core-1] += t_in.get_total();
+		});};
+		auto peelAllFixB = [&](){
+		par_for(breakptrs[idx-1]+1, breakptrs[idx]+1, 1, [&](size_t core){
+			timer t_in; t_in.start();
+			sequence<uintE> D = prepeelB[idx];
+			size_t initSize = pbbslib::reduce_add(sequence<uintE>(n, [&](size_t i) { return (D[i]<core) & (D[i]>=minCore); }));
+			std::vector<uintE> delB(initSize);
+			for(size_t i = 0, j = 0; i<n; i++) if((D[i]<core) && (D[i]>=minCore)) delB[j++] = i;
+			initialClean(G, D, delB, core);
+
+			auto ret = PeelFixB(G, BetaMax, AlphaMax, D, core, bipartition, num_buckets);
+			timeB[core-1] = std::get<1>(ret);
+			t_in.stop();
+			tTime[core-1] += t_in.get_total();
+		});};
+		par_do(peelAllFixA,peelAllFixB);
+		std::cout<<"range "<<breakptrs[idx-1]+1<<" to "<<breakptrs[idx]<<" finished"<<std::endl;
+	});
+
 }
 
 template <class Graph>
-inline std::pair<double, double> PeelFixA(Graph& G, sequence<uintE> D, uintE alpha, size_t n_a, size_t n_b, size_t num_buckets)
+inline std::pair<double, double> PeelFixA(Graph& G, sequence<uintE>& D, uintE alpha, size_t n_a, size_t n_b, size_t num_buckets)
 {
 	const size_t n = n_a + n_b;
 	timer pqt, pt;
