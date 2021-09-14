b3c77df0b81ea9ee444647726452feded349d039
Michael Huang <yhuang23@andover.edu>
2021-09-14 15:24:31 +0000

    outputfiles

--- benchmarks/BiCore/trainer.sh
+++ benchmarks/BiCore/trainer.sh
@@ -1,55 +1,37 @@
 export NUM_THREADS=60
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_60
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_60
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_60
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_60
 
 export NUM_THREADS=30
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_30
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_30
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_30
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_30
 
 export NUM_THREADS=24
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_24
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_24
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_24
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_24
 
 export NUM_THREADS=16
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_16
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_16
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_16
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_16
 
 export NUM_THREADS=12
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_12
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_12
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_12
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_12
 
 export NUM_THREADS=8
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_8
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_8
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_8
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_8
 
 export NUM_THREADS=4
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_4
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_4
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_4
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_4
 
 export NUM_THREADS=2
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_2
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_2
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_2
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_2
 
 export SERIAL=1
 make
-./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_1
-./BiCore -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_1
-./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_1
+./BiCore -s -bi 556076 -rounds 1 ../../inputs/trec_adj > trec_1
 
 sudo shutdown
