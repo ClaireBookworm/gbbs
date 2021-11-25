f7f3012988403cf3a59552fa08fd5e9d7ca8c917
Michael Huang <michaelyhuang23@gmail.com>
2021-11-25 02:38:09 -0500

    change trainer

--- benchmarks/BiCoreIndex/collect_data.py
+++ benchmarks/BiCoreIndex/collect_data.py
@@ -6,23 +6,22 @@ dicq = {}
 
 for file in os.listdir():
 	if ".txt" not in file: continue
-	if "orkut" in file : continue
 	name = file.split("_")[0]
 	num = int(file.split("_")[1])
 	with open(file, 'r') as f:
 		lines = f.readlines()
 	qlines = [line for line in lines if "Query" in line]
 	qnums = [float(line[line.index("Query")+12 : -1]) for line in qlines]
-	assert(len(qnums)==3)
+	assert(len(qnums)<=3)
 	clines = [line for line in lines if "Construction" in line]
 	cnums = [float(line[line.index("Construction")+19 : -1]) for line in clines]
-	assert(len(cnums)==3)
+	assert(len(cnums)<=3)
 	if name not in dicc:
 		dicc[name] = {}
 	if name not in dicq:
 		dicq[name] = {}
-	dicc[name][num] = sum(cnums)/3
-	dicq[name][num] = sum(qnums)/3
+	dicc[name][num] = sum(cnums)/len(cnums)
+	dicq[name][num] = sum(qnums)/len(qnums)
 
 
 print("construction times:")
--- benchmarks/BiCoreIndex/trainer.sh
+++ benchmarks/BiCoreIndex/trainer.sh
@@ -1,118 +1,39 @@
-
-unset SERIAL
 unset NUM_THREADS
+unset SERIAL
+export NUM_THREADS=60
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_60_idx.txt
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_60
 
 export NUM_THREADS=30
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_30_idx.txt
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_30
 
 export NUM_THREADS=24
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_24_idx.txt
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_24
 
 export NUM_THREADS=16
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_16_idx.txt
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_16
 
 export NUM_THREADS=12
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_12_idx.txt
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_12
 
 export NUM_THREADS=8
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_8_idx.txt
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_8
 
 export NUM_THREADS=4
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_4_idx.txt
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_4
 
 export NUM_THREADS=2
 make
-./BiCoreIndex -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_2_idx.txt
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_2
 
 export SERIAL=1
 make
-./BiCoreIndex -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_1_idx.txt
-
-
-
-unset SERIAL
-unset NUM_THREADS
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_60_idx.txt
-
-export NUM_THREADS=30
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_30_idx.txt
-
-export NUM_THREADS=24
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_24_idx.txt
-
-export NUM_THREADS=16
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_16_idx.txt
-
-export NUM_THREADS=12
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_12_idx.txt
-
-export NUM_THREADS=8
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_8_idx.txt
-
-export NUM_THREADS=4
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_4_idx.txt
-
-export NUM_THREADS=2
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_2_idx.txt
-
-export SERIAL=1
-make
-./BiCoreIndex -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_1_idx.txt
-
-
-
-unset SERIAL
-unset NUM_THREADS
-make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_60_idx.txt
-
-export NUM_THREADS=30
-make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_30_idx.txt
-
-export NUM_THREADS=24
-make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_24_idx.txt
-
-export NUM_THREADS=16
-make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_16_idx.txt
-
-export NUM_THREADS=12
-make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_12_idx.txt
-
-export NUM_THREADS=8
-make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_8_idx.txt
-
-export NUM_THREADS=4
-make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_4_idx.txt
-
-export NUM_THREADS=2
-make
-./BiCoreIndex -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_2_idx.txt
-
-export SERIAL=1
-make
-./BiCoreIndex -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_1_idx.txt
-
+./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_1
 
 sudo shutdown
