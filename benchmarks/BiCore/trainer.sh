1da10cdf7ce0f0c6adfa570a33e7aa2780b25d0c
Michael Huang <michaelyhuang23@gmail.com>
2021-11-25 03:37:39 -0500

    new trainer

--- benchmarks/BiCore/trainer.sh
+++ benchmarks/BiCore/trainer.sh
@@ -1,82 +1,8 @@
-export NUM_THREADS=60
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_60
-
-export NUM_THREADS=30
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_30
-
-export NUM_THREADS=24
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_24
-
-export NUM_THREADS=16
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_16
-
-export NUM_THREADS=12
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_12
-
-export NUM_THREADS=8
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_8
-
-export NUM_THREADS=4
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_4
-
-export NUM_THREADS=2
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_2
-
-export SERIAL=1
-make
-./BiCore -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_1
-
-unset SERIAL
-export NUM_THREADS=60
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_60
-
-export NUM_THREADS=30
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_30
-
-export NUM_THREADS=24
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_24
-
-export NUM_THREADS=16
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_16
-
-export NUM_THREADS=12
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_12
-
-export NUM_THREADS=8
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_8
-
-export NUM_THREADS=4
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_4
-
-export NUM_THREADS=2
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_2
-
-export SERIAL=1
-make
-./BiCore -s -bi 120491 -rounds 3 ../../inputs/epinion_adj > epinion_1
-
 unset SERIAL
 export NUM_THREADS=60
 make
 ./BiCore -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_60
 
-unset SERIAL
 export NUM_THREADS=30
 make
 ./BiCore -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_30
@@ -99,52 +25,16 @@ make
 
 export NUM_THREADS=4
 make
-./BiCore -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_4
+./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_4
 
 export NUM_THREADS=2
 make
-./BiCore -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_2
+./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_2
 
 export SERIAL=1
 make
-./BiCore -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_1
+./BiCore -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_1
 
-unset SERIAL
-export NUM_THREADS=60
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_60
-
-export NUM_THREADS=30
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_30
-
-export NUM_THREADS=24
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_24
-
-export NUM_THREADS=16
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_16
-
-export NUM_THREADS=12
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_12
-
-export NUM_THREADS=8
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_8
-
-export NUM_THREADS=4
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_4
-
-export NUM_THREADS=2
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_2
-
-export SERIAL=1
-make
-./BiCore -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_1
 
 unset SERIAL
 export NUM_THREADS=60
@@ -210,15 +100,15 @@ make
 
 export NUM_THREADS=4
 make
-./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_4
+./BiCore -s -bi 781264 -rounds 1 ../../inputs/reuter_adj > reuter_4
 
 export NUM_THREADS=2
 make
-./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_2
+./BiCore -s -bi 781264 -rounds 1 ../../inputs/reuter_adj > reuter_2
 
 export SERIAL=1
 make
-./BiCore -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_1
+./BiCore -s -bi 781264 -rounds 1 ../../inputs/reuter_adj > reuter_1
 
 unset SERIAL
 export NUM_THREADS=60
@@ -247,14 +137,14 @@ make
 
 export NUM_THREADS=4
 make
-./BiCore -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_4
+./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_4
 
 export NUM_THREADS=2
 make
-./BiCore -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_2
+./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_2
 
 export SERIAL=1
 make
-./BiCore -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_1
+./BiCore -s -bi 2783195 -rounds 1 ../../inputs/orkut_adj > orkut_1
 
 sudo shutdown
