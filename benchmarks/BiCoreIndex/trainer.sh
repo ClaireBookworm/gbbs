c50d574537081bfdb7daa138963da9724007f46f
Michael Huang <michaelyhuang23@gmail.com>
2021-11-25 10:01:28 +0000

    flickr log

--- benchmarks/BiCoreIndex/trainer.sh
+++ benchmarks/BiCoreIndex/trainer.sh
@@ -2,38 +2,38 @@ unset NUM_THREADS
 unset SERIAL
 export NUM_THREADS=60
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_60_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_60_idx.txt
 
 export NUM_THREADS=30
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_30_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_30_idx.txt
 
 export NUM_THREADS=24
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_24_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_24_idx.txt
 
 export NUM_THREADS=16
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_16_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_16_idx.txt
 
 export NUM_THREADS=12
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_12_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_12_idx.txt
 
 export NUM_THREADS=8
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_8_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_8_idx.txt
 
 export NUM_THREADS=4
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_4_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_4_idx.txt
 
 export NUM_THREADS=2
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_2_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_2_idx.txt
 
 export SERIAL=1
 make
-./BiCore -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_1_idx.txt
+./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_1_idx.txt
 
 sudo shutdown
