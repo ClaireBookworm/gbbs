ea6822bbfd9a754cc6c875662499dbcd7ef00dff
Michael Huang <michaelyhuang23@gmail.com>
2021-11-25 05:09:30 +0000

    new log

--- benchmarks/BiCoreIndex/trainer.sh
+++ benchmarks/BiCoreIndex/trainer.sh
@@ -1,157 +1,3 @@
-unset SERIAL
-unset NUM_THREADS
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_60_idx.txt
-
-export NUM_THREADS=30
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_30_idx.txt
-
-export NUM_THREADS=24
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_24_idx.txt
-
-export NUM_THREADS=16
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_16_idx.txt
-
-export NUM_THREADS=12
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_12_idx.txt
-
-export NUM_THREADS=8
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_8_idx.txt
-
-export NUM_THREADS=4
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_4_idx.txt
-
-export NUM_THREADS=2
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_2_idx.txt
-
-export SERIAL=1
-make
-./BiCoreIndex -s -bi 556076 -rounds 3 ../../inputs/trec_adj > trec_1_idx.txt
-
-
-unset SERIAL
-unset NUM_THREADS
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_60_idx.txt
-
-export NUM_THREADS=30
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_30_idx.txt
-
-export NUM_THREADS=24
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_24_idx.txt
-
-export NUM_THREADS=16
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_16_idx.txt
-
-export NUM_THREADS=12
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_12_idx.txt
-
-export NUM_THREADS=8
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_8_idx.txt
-
-export NUM_THREADS=4
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_4_idx.txt
-
-export NUM_THREADS=2
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_2_idx.txt
-
-export SERIAL=1
-make
-./BiCoreIndex -s -bi 395978 -rounds 3 ../../inputs/flickr_adj > flickr_1_idx.txt
-
-
-
-unset SERIAL
-unset NUM_THREADS
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_60_idx.txt
-
-export NUM_THREADS=30
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_30_idx.txt
-
-export NUM_THREADS=24
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_24_idx.txt
-
-export NUM_THREADS=16
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_16_idx.txt
-
-export NUM_THREADS=12
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_12_idx.txt
-
-export NUM_THREADS=8
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_8_idx.txt
-
-export NUM_THREADS=4
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_4_idx.txt
-
-export NUM_THREADS=2
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_2_idx.txt
-
-export SERIAL=1
-make
-./BiCoreIndex -s -bi 781264 -rounds 3 ../../inputs/reuter_adj > reuter_1_idx.txt
-
-
-
-unset SERIAL
-unset NUM_THREADS
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_60_idx.txt
-
-export NUM_THREADS=30
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_30_idx.txt
-
-export NUM_THREADS=24
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_24_idx.txt
-
-export NUM_THREADS=16
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_16_idx.txt
-
-export NUM_THREADS=12
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_12_idx.txt
-
-export NUM_THREADS=8
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_8_idx.txt
-
-export NUM_THREADS=4
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_4_idx.txt
-
-export NUM_THREADS=2
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_2_idx.txt
-
-export SERIAL=1
-make
-./BiCoreIndex -s -bi 2783195 -rounds 3 ../../inputs/orkut_adj > orkut_1_idx.txt
-
-
 
 unset SERIAL
 unset NUM_THREADS
@@ -184,11 +30,11 @@ make
 
 export NUM_THREADS=2
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_2_idx.txt
+./BiCoreIndex -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_2_idx.txt
 
 export SERIAL=1
 make
-./BiCoreIndex -s -bi 27665729 -rounds 3 ../../inputs/tracker_adj > tracker_1_idx.txt
+./BiCoreIndex -s -bi 27665729 -rounds 1 ../../inputs/tracker_adj > tracker_1_idx.txt
 
 
 
@@ -262,11 +108,11 @@ make
 
 export NUM_THREADS=2
 make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_2_idx.txt
+./BiCoreIndex -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_2_idx.txt
 
 export SERIAL=1
 make
-./BiCoreIndex -s -bi 3201202 -rounds 3 ../../inputs/journal_adj > journal_1_idx.txt
+./BiCoreIndex -s -bi 3201202 -rounds 1 ../../inputs/journal_adj > journal_1_idx.txt
 
 
 sudo shutdown
